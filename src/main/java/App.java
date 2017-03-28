029218eab68d94b5b0b505c806f70a98b1dc3c59
Chris Finney <the.finney@gmail.com>
2017-03-28 09:26:24 -0700

    revise UI for 2-player competition

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,12 +5,37 @@ public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
 
-    System.out.println("Please enter a scrabble word!");
-    String stringUserInput = myConsole.readLine();
-    System.out.println("-------------------------------");
-  Scrabble scrabble = new Scrabble();
-  Integer scrabbleScore = scrabble.runScrabble(stringUserInput);
-  System.out.println(scrabbleScore);
-
+    boolean programRunning = true;
+    while (programRunning) {
+    System.out.println("-----------------------------------\nPlayer 1, please enter a scrabble word!");
+    System.out.println("-----------------------------------");
+    String playerOneInput = myConsole.readLine();
+    Scrabble scrabbleOne = new Scrabble();
+    Integer playerOneScore = scrabbleOne.runScrabble(playerOneInput);
+    System.out.println("-----------------------------------\nPlayer 2, please enter a scrabble word!");
+    System.out.println("-----------------------------------");
+    String playerTwoInput = myConsole.readLine();
+    Scrabble scrabbleTwo = new Scrabble();
+    Integer playerTwoScore = scrabbleTwo.runScrabble(playerTwoInput);
+    System.out.println("\n...................................");
+    System.out.println("\nSCOREBOARD:\n\nPlayer 1 scored " + playerOneScore + " points for the word '" + playerOneInput + "'.\n\nPlayer 2 scored " + playerTwoScore + " points for the word '" + playerTwoInput + "'.");
+    System.out.println("...................................\n\n");
+    System.out.println("<><><><><><><><><><><><><><><><><><>");
+      if (playerOneScore > playerTwoScore) {
+        System.out.println("Player 1 is the winner!");
+      } else if (playerTwoScore > playerOneScore) {
+        System.out.println("Player 2 is the winner!");
+      } else {
+        System.out.println("Looks like we have a tie. Of course you have to play again to settle this score!");
+      }
+    System.out.println("<><><><><><><><><><><><><><><><><><>");
+    System.out.println("\nPlay again? Please enter yes or no.");
+    String playAgain = myConsole.readLine().toLowerCase();
+      if (playAgain.startsWith("y")) {
+        programRunning = true;
+      } else if (playAgain.startsWith("n")) {
+        programRunning = false;
+      }
+    }
   }
 }
--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -5,37 +5,29 @@ import java.util.Arrays;
 public class Scrabble {
 
   public Integer runScrabble(String stringUserInput){
-    String[] arrayValue1 = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
-    List oneValueLetter = Arrays.asList(arrayValue1);
-    String[] arrayValue2 = {"d", "g"};
-    List twoValueLetter = Arrays.asList(arrayValue2);
-    String[] arrayValue3 = {"b", "c", "m", "p"};
-    List threeValueLetter = Arrays.asList(arrayValue3);
-    String[] arrayValue4 = {"f", "h", "v", "y"};
-    List fourValueLetter = Arrays.asList(arrayValue4);
-    String[] arrayValue5 = {"k"};
-    List fiveValueLetter = Arrays.asList(arrayValue5);
-    String[] arrayValue8 = {"j", "x"};
-    List eightValueLetter = Arrays.asList(arrayValue8);
-    String[] arrayValue10 = {"q", "z"};
-    List tenValueLetter = Arrays.asList(arrayValue10);
+    List<String> arrayValue1 = Arrays.asList("a", "e", "i", "o", "u", "l", "n", "r", "s", "t");
+    List<String> arrayValue2 = Arrays.asList("d", "g");
+    List<String> arrayValue3 = Arrays.asList("b", "c", "m", "p");
+    List<String> arrayValue4 = Arrays.asList("f", "h", "v", "y");
+    List<String> arrayValue5 = Arrays.asList("k");
+    List<String> arrayValue8 = Arrays.asList("j", "x");
+    List<String> arrayValue10 = Arrays.asList("q", "z");
 
-    String[] userInput = stringUserInput.toLowerCase().split("");
-    List userWordArray = Arrays.asList(userInput);
+    List<String> userWordArray = Arrays.asList(stringUserInput.toLowerCase().split(""));
 
     Integer score = 0;
     for (int i = 0; i < userWordArray.size(); i++)
-      if (oneValueLetter.contains(userWordArray.get(i))) {
+      if (arrayValue1.contains(userWordArray.get(i))) {
         score += 1;
-      } else if (twoValueLetter.contains(userWordArray.get(i))) {
+      } else if (arrayValue2.contains(userWordArray.get(i))) {
         score += 2;
-      }else if (threeValueLetter.contains(userWordArray.get(i))) {
+      }else if (arrayValue3.contains(userWordArray.get(i))) {
         score += 3;
-      }else if (fourValueLetter.contains(userWordArray.get(i))) {
+      }else if (arrayValue4.contains(userWordArray.get(i))) {
         score += 4;
-      }else if (fiveValueLetter.contains(userWordArray.get(i))) {
+      }else if (arrayValue5.contains(userWordArray.get(i))) {
         score += 5;
-      }else if (eightValueLetter.contains(userWordArray.get(i))) {
+      }else if (arrayValue8.contains(userWordArray.get(i))) {
         score += 8;
       }else{
         score += 10;
@@ -43,27 +35,3 @@ public class Scrabble {
       return score;
   }
 }
-
-    // String[] arrayUserInput = stringUserInput.split(" ");
-    // for (int i = 0; i < arrayUserInput.length; i++) {
-    //   if (arrayUserInput[i].equals(arrayValue1)) {
-    //     expected += 1
-    //   }
-    // }
-
-      // return expected;
-//   }
-// }
-
-// take user input (e.g. word), split "word" into
-
-// stringUserInput.split(' ')
-
-// char[] charArray = stringUserInput.toCharArray();
-// Character[] charObjectArray = ArrayUtils.toObject(charArray);
-//
-// for (int i = 0 ; i < stringUserInput.length(); i++){
-//   if (arrayValue1.contains(stringUserInput.charAt(i))){
-//     expected += 1;
-//
-//     for (String element : arrayUserInput)
